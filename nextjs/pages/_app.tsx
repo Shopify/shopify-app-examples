26893f97a0108c3ef39ca55dc45c5803c7fde3c8
Ryan Keller <ryan.keller@shopify.com>
2022-05-26 14:11:00 -0500

    feat: env vars, app bridge utils, hooks

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,12 +1,84 @@
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../apollo/client'
+import { useMemo } from 'react';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import {
+  Provider as AppBridgeProvider,
+  useAppBridge,
+} from "@shopify/app-bridge-react";
+import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { Redirect } from "@shopify/app-bridge/actions";
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
+import translations from "@shopify/polaris/locales/en.json";
+import { useRouteChangeLoader } from "../hooks/useRouteChangeLoader";
+import { fetch } from "../app-bridge/fetch";
+import { Link } from '../components/Link';
+import { RoutePropagator } from '../components/RoutePropagator';
+import "@shopify/polaris/build/esm/styles.css";
 
-export default function App({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
+
+// import { useApollo } from '../apollo/client'
+
+/**
+ * React Apollo Context Provider configuration
+ * Done as a separate component so we could use App Bridge Context
+ * App Bridge is configured and made available bellow in App component
+ *
+ * More on Apollo Context Provider:
+ * https://www.apollographql.com/docs/react/api/react/hooks/#the-apolloprovider-component
+ */
+function ConfiguredApolloProvider({ children }) {
+  const app = useAppBridge();
+
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        // configuring custom fetch so we could have reusable App Bridge logic on requests
+        fetch: fetch(app),
+        fetchOptions: {
+          credentials: "include",
+        },
+        cache: new InMemoryCache(),
+      } as any),
+    [app]
+  );
+
+  useRouteChangeLoader();
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
+
+function App({ Component, pageProps, host = 'localhost' }) {
+  // const apolloClient = useApollo(pageProps.initialApolloState)
+  console.warn('HOST', host, pageProps)
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+      <AppBridgeProvider
+      config={
+        {
+          host,
+          apiKey: process.env.SHOPIFY_API_KEY,
+          forceRedirect: true,
+        }
+      }>
+
+        <RoutePropagator />
+        <PolarisProvider i18n={translations} linkComponent={Link}>
+            <ConfiguredApolloProvider>
+              <Component {...pageProps} />
+            </ConfiguredApolloProvider >
+        </PolarisProvider>
+      </AppBridgeProvider>
   )
 }
+
+App.getInitialProps = async ({ ctx }) => {
+  return {
+    host: ctx.query.host,
+  };
+};
+
+export default App;
--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -15,32 +15,18 @@ const ViewerQuery = gql`
 
 const Index = () => {
   const {
-    data: { viewer },
+    data
   } = useQuery(ViewerQuery)
 
   return (
     <div>
-      You're signed in as {viewer.name} and you're {viewer.status} goto{' '}
+      You're signed in as {data?.viewer.name} and you're {data?.viewer.status} goto{' '}
       <Link href="/about">
         <a>static</a>
       </Link>{' '}
       page.
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-
-  await apolloClient.query({
-    query: ViewerQuery,
-  })
-
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-  }
 }
 
 export default Index
