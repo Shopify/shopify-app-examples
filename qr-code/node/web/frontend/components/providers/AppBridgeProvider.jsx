e4f97e14e69ec0c6411db1ffc59022de701312aa
Jason Miller <jason.miller@shopify.com>
2022-05-30 14:43:29 -0400

    Fix Vite reloads crashing AppBridge

--- qr-code/node/web/frontend/components/providers/AppBridgeProvider.jsx
+++ qr-code/node/web/frontend/components/providers/AppBridgeProvider.jsx
@@ -2,6 +2,8 @@ import { useMemo, useRef } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Provider } from '@shopify/app-bridge-react'
 
+const APPBRIDGE_HOST = new URLSearchParams(location.search).get('host')
+
 export function AppBridgeProvider({ children }) {
   const location = useLocation()
   const navigate = useNavigate()
@@ -19,15 +21,11 @@ export function AppBridgeProvider({ children }) {
     [history, location]
   )
 
-  const { current: host } = useRef(
-    new URL(window.location).searchParams.get('host')
-  )
-
   return (
     <Provider
       config={{
         apiKey: process.env.SHOPIFY_API_KEY,
-        host,
+        host: APPBRIDGE_HOST,
         forceRedirect: true,
       }}
       router={routerConfig}
