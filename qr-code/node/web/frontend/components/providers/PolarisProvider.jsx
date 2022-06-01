63da889f1d93a53c67985227ef9216f492f877a7
Richard Powell <richard.powell@shopify.com>
2022-06-01 10:30:54 -0400

    Copy generic hooks and components from the template
    Most of these changes are just doc blocks

--- qr-code/node/web/frontend/components/providers/PolarisProvider.jsx
+++ qr-code/node/web/frontend/components/providers/PolarisProvider.jsx
@@ -27,6 +27,26 @@ function AppBridgeLink({ url, children, className, external, ...rest }) {
   )
 }
 
+/**
+ * Sets up the AppProvider from Polaris.
+ * @desc PolarisProvider passes a custom link component to Polaris.
+ * The Link component handles navigation within an embedded app.
+ * Prefer using this vs any other method such as an anchor.
+ * Use it by importing Link from Polaris, e.g:
+ *
+ * ```
+ * import {Link} from '@shopify/polaris'
+ *
+ * function MyComponent() {
+ *  return (
+ *    <div><Link url="/tab2">Tab 2</Link></div>
+ *  )
+ * }
+ * ```
+ *
+ * PolarisProvider also passes translations to Polaris.
+ *
+ */
 export function PolarisProvider({ children }) {
   return (
     <AppProvider i18n={translations} linkComponent={AppBridgeLink}>
