8ca22a266c98d395c1ab65254d4ecbded0b56422
Richard Powell <richard.powell@shopify.com>
2022-06-01 10:36:42 -0400

    Copy the latest tests for Routes from the template into the example app

--- qr-code/node/web/frontend/Routes.test.jsx
+++ qr-code/node/web/frontend/Routes.test.jsx
@@ -6,6 +6,7 @@ import Routes from './Routes'
 
 const Index = () => null
 const CamelCase = () => null
+const NotFound = () => null
 
 const pages = {
   './pages/index.jsx': {
@@ -25,6 +26,9 @@ const pages = {
   },
   './pages/CamelCase.jsx': {
     default: CamelCase,
+  },
+  './pages/NotFound.jsx': {
+    default: NotFound,
   },
 }
 
@@ -63,13 +67,16 @@ it('normalizes routes to lowercase', async () => {
 it('warns when a page has no default export', async () => {
   vi.spyOn(console, 'warn').mockImplementation(() => {})
 
-  const pages = {
-    './comments.jsx': {
-      Comments: () => null,
-    },
-  }
-
-  const component = await mount(<Routes pages={pages} />)
+  const component = await mount(
+    <Routes
+      pages={{
+        ...pages,
+        './comments.jsx': {
+          Comments: () => null,
+        },
+      }}
+    />
+  )
 
   expect(console.warn).toHaveBeenCalledWith(
     "./comments.jsx doesn't export a default React component"
