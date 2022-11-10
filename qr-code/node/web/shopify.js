68cec816fa656bff91438c35420b50cba1492892
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-11-10 10:57:07 -0500

    Update express package to rc4

--- qr-code/node/web/shopify.js
+++ qr-code/node/web/shopify.js
@@ -1,7 +1,7 @@
 import { LogSeverity } from "@shopify/shopify-api";
 import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
 import { shopifyApp } from "@shopify/shopify-app-express";
-import { SQLiteSessionStorage } from "@shopify/shopify-app-express/dist/src/session-storage/sqlite.js";
+import { SQLiteSessionStorage } from "@shopify/shopify-app-express/session-storage/sqlite";
 
 import { join } from "path";
 import { QRCodesDB } from "./qr-codes-db.js";
@@ -15,11 +15,7 @@ QRCodesDB.init();
 const shopify = shopifyApp({
   api: {
     restResources,
-    logger: {
-      level: LogSeverity.Warning,
-    },
   },
-  // This should be replaced with your preferred storage strategy
   sessionStorage: sessionDb,
 });
 
